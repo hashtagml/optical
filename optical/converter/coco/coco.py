3ef154c27676ae200c3807eb1d92545c008aafc5
bishwarup307 <write2bishwarup@gmail.com>
2021-03-31 17:28:34 +0530

    add formatspec and coco
    update readme
    update dependencies
    add base.py
    add vscode workspace settings

--- optical/converter/coco/coco.py
+++ optical/converter/coco/coco.py
@@ -4,21 +4,20 @@ license: MIT
 Created: Monday, 29th March 2021
 """
 
-from typing import Union, List, Dict
+
 import os
-from pathlib import Path
 import warnings
-from joblib import Parallel, delayed
+from pathlib import Path
+from typing import List, Union
+
 import pandas as pd
+import numpy as np
 
-from ..utils import exists, get_image_dir, get_annotation_dir, read_coco
-
-
-def extract_info(annot: Dict):
-    return annot["category_id"], annot["bbox"]
+from ..base import FormatSpec
+from ..utils import exists, get_annotation_dir, get_image_dir, read_coco
 
 
-class COCO:
+class COCO(FormatSpec):
     def __init__(self, root: Union[str, os.PathLike]):
         self.root = root
         self._image_dir = get_image_dir(root)
@@ -26,12 +25,12 @@ class COCO:
         assert exists(self._image_dir), "root is missing `images` directory."
         assert exists(self._annotation_dir), "root is missing `annotations` directory."
         self._splits = self._find_splits()
+        self._resolve_dataframe()
 
     def _find_splits(self):
         im_splits = [x.name for x in Path(self._image_dir).iterdir() if x.is_dir()]
         ann_splits = [x.stem for x in Path(self._annotation_dir).glob("*.json")]
-        print(im_splits)
-        print(ann_splits)
+
         no_anns = set(im_splits).difference(ann_splits)
         if no_anns:
             warnings.warn(f"no annotation found for {', '.join(list(no_anns))}")
@@ -43,21 +42,52 @@ class COCO:
             class_map[cat["id"]] = cat["name"]
         return class_map
 
-    def describe(self, classwise: bool = False):
+    def _resolve_dataframe(self):
         split_str = []
+        master_df = pd.DataFrame(
+            columns=["image_id", "image_width", "image_height", "x_min", "y_min", "width", "height", "category"],
+        )
         for split in self._splits:
+            # info = DotMap()
+
             coco_json = self._annotation_dir / f"{split}.json"
             images, annots, cats = read_coco(coco_json)
-            split_str.append([len(images), len(annots), len(cats)])
+            split_str.append([split, len(images), len(annots), len(cats)])
 
-            # if classwise:
             class_map = self._get_class_map(cats)
-            info = Parallel(n_jobs=-1, backend="threading")(delayed(extract_info)(ann) for ann in annots)
-            # a, b = [], []
-            # for ann in annots:
-            #     a.append(ann["category_id"])
-            #     b.append(ann["bbox"])
 
-            # df = pd.DataFrame({"class_id": a, "bbox": b})
-            df = pd.DataFrame(info, columns=["class_id", "bbox"])
-            setattr(self, split, df)
+            images_df = pd.DataFrame(images)
+            images_df = images_df[["id", "file_name", "width", "height"]]
+            images_df.rename(columns={"width": "image_width", "height": "image_height"}, inplace=True)
+
+            instances = [(x["image_id"], x["category_id"], x["bbox"]) for x in annots]
+            annots_df = pd.DataFrame(instances, columns=["image_id", "class_id", "bbox"])
+            annots_df["category"] = annots_df["class_id"].map(class_map)
+            annots_df[["x_min", "y_min", "width", "height"]] = pd.DataFrame(
+                annots_df["bbox"].to_list(), index=annots_df.index
+            )
+
+            annots_df.drop(["bbox"], axis=1, inplace=True)
+
+            annots_df = annots_df.merge(images_df, left_on="image_id", right_on="id", how="left")
+            annots_df.drop(["id", "image_id"], axis=1, inplace=True)
+            annots_df.rename(columns={"file_name": "image_id"}, inplace=True)
+            annots_df["split"] = split
+
+            if len(annots_df[pd.isnull(annots_df.image_id)]) > 0:
+                warnings.warn(
+                    "There are annotations in your dataset for which there is no matching images"
+                    + f"(in split `{split}`). These annotations will be removed during any "
+                    + "computation or conversion. It is recommended that you clean your dataset."
+                )
+
+            master_df = pd.concat([master_df, annots_df], ignore_index=True)
+
+        master_df = master_df[pd.notnull(master_df.image_id)]
+        for col in ["x_min", "y_min", "width", "height"]:
+            master_df[col] = master_df[col].astype(np.float32)
+
+        for col in ["image_width", "image_height"]:
+            master_df[col] = master_df[col].astype(np.int32)
+
+        self.master_df = master_df
