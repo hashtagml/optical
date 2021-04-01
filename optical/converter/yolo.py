0fa248f393ec8043ae47325019c39c6c1eb7e7b8
bishwarup307 <write2bishwarup@gmail.com>
2021-04-01 23:22:55 +0530

    add Annotation class

---
+++ optical/converter/yolo.py
@@ -0,0 +1,17 @@
+"""
+__author__: HashTagML
+license: MIT
+Created: Wednesday, 31st March 2021
+"""
+
+from .base import FormatSpec
+from typing import Union
+import os
+
+
+class Yolo(FormatSpec):
+    def __init__(self, root: Union[str, os.PathLike]):
+        pass
+
+    def _resolve_dataframe(self):
+        return super()._resolve_dataframe()
